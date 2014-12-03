1d57f319b7ba2fe6af86e537698fb4ed03789ec0
Jason Murray <jason@multisafepay.com>
2014-12-03 09:25:01 +0100

    Added tests for UrlProvider. Need to update the implementation of UrlProvider so it less naive.

---
+++ Tests/MultiSafepay.UnitTests/UrlProviderTests.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MultiSafepay.UnitTests
+{
+    /// <summary>
+    /// Tests that the UrlProvider returns the correct resource urls for each resource.
+    /// Also checks that correct resources urls are returned whether or not a trailing slash is specified.
+    /// </summary>
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class UrlProviderTests
+    {
+        private readonly string[] _baseUrls = { "https://api.multisafepay.com/", "https://api.multisafepay.com" };
+
+        [TestMethod]
+        public void GatewaysUrl()
+        {
+            foreach (var baseUrl in _baseUrls)
+            {
+                // Arrange
+                var provider = new UrlProvider(baseUrl);
+
+                // Act
+                var url = provider.GatewaysUrl();
+
+                // Assert
+                Assert.AreEqual(String.Format("{0}/gateways", baseUrl.TrimTrailingSlash()), url);
+            }
+        }
+
+        [TestMethod]
+        public void GatewayUrl()
+        {
+            foreach (var baseUrl in _baseUrls)
+            {
+                // Arrange
+                var provider = new UrlProvider(baseUrl);
+                const string name = " test";
+
+                // Act
+                var url = provider.GatewayUrl(name);
+
+                // Assert
+                Assert.AreEqual(String.Format("{0}/gateways/{1}", baseUrl.TrimTrailingSlash(), name), url);
+            }
+        }
+
+        [TestMethod]
+        public void IssuersUrl()
+        {
+            foreach (var baseUrl in _baseUrls)
+            {
+                // Arrange
+                var provider = new UrlProvider(baseUrl);
+                const string name = " test";
+
+                // Act
+                var url = provider.IssuersUrl(name);
+
+                // Assert
+                Assert.AreEqual(String.Format("{0}/issuers/{1}", baseUrl.TrimTrailingSlash(), name), url);
+            }
+        }
+
+        [TestMethod]
+        public void OrderUrl()
+        {
+            foreach (var baseUrl in _baseUrls)
+            {
+                // Arrange
+                var provider = new UrlProvider(baseUrl);
+                const string orderid = " test";
+
+                // Act
+                var url = provider.OrderUrl(orderid);
+
+                // Assert
+                Assert.AreEqual(String.Format("{0}/orders/{1}", baseUrl.TrimTrailingSlash(), orderid), url);
+            }
+        }
+
+        [TestMethod]
+        public void TransactionUrl()
+        {
+            foreach (var baseUrl in _baseUrls)
+            {
+                // Arrange
+                var provider = new UrlProvider(baseUrl);
+                const string transactionId = " test";
+
+                // Act
+                var url = provider.TransactionUrl(transactionId);
+
+                // Assert
+                Assert.AreEqual(String.Format("{0}/transactions/{1}", baseUrl.TrimTrailingSlash(), transactionId), url);
+            }
+        }
+
+        [TestMethod]
+        public void PaymentLinkUrl()
+        {
+            foreach (var baseUrl in _baseUrls)
+            {
+                // Arrange
+                var provider = new UrlProvider(baseUrl);
+                const string orderId = " test";
+
+                // Act
+                var url = provider.PaymentLinkUrl(orderId);
+
+                // Assert
+                Assert.AreEqual(String.Format("{0}/orders/{1}/paymentlink", baseUrl.TrimTrailingSlash(), orderId), url);
+            }
+        }
+
+        [TestMethod]
+        public void OrdersUrl()
+        {
+            foreach (var baseUrl in _baseUrls)
+            {
+                // Arrange
+                var provider = new UrlProvider(baseUrl);
+
+                // Act
+                var url = provider.OrdersUrl();
+
+                // Assert
+                Assert.AreEqual(String.Format("{0}/orders", baseUrl.TrimTrailingSlash()), url);
+            }
+        }
+
+        [TestMethod]
+        public void OrderRefundsUrl()
+        {
+            foreach (var baseUrl in _baseUrls)
+            {
+                // Arrange
+                var provider = new UrlProvider(baseUrl);
+                const string orderId = " test";
+
+                // Act
+                var url = provider.OrderRefundsUrl(orderId);
+
+                // Assert
+                Assert.AreEqual(String.Format("{0}/orders/{1}/refunds", baseUrl.TrimTrailingSlash(), orderId), url);
+            }
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static string TrimTrailingSlash(this string input)
+        {
+            if (input.EndsWith("/"))
+            {
+                return input.Substring(0, input.Length - 1);
+            }
+            return input;
+        }
+    }
+}
